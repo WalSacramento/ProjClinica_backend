185238c99b0d744b7c0be3b65d02177f243dbfee
Waldsson Sacramento <waldsson.dev@gmail.com>
2023-08-20 23:00:24 -0300

    Melhorias em DashboardController

--- src/controller/DashboardController.js
+++ src/controller/DashboardController.js
@@ -18,6 +18,7 @@ export default {
 
     try {
       const clinic = await prisma.clinica.findUnique({ where: { id: Number(id) } })
+
       const consults = await prisma.consulta.findMany({
         where: {
           data_de_criacao: {
@@ -54,7 +55,7 @@ export default {
 
       function groupConsultsByProfissional(consults) {
         const consultsByProfissional = {};
-      
+
         consults.forEach(consult => {
           const profissionalName = consult.profissional.nome;
           if (!consultsByProfissional[profissionalName]) {
@@ -62,7 +63,7 @@ export default {
           }
           consultsByProfissional[profissionalName].push(consult);
         });
-      
+
         return consultsByProfissional;
       }
 
@@ -75,11 +76,98 @@ export default {
         return { profissional, numberOfConsults, totalValue };
       })
 
-      return res.json(valueForProfissional)
+      const dailyData = {
+        Clinica: clinic.nome,
+        Total_de_consultas: numberOfConsults,
+        Valor_total: totalValue,
+        Valor_por_profissional: valueForProfissional
+      }
+
+      return res.json(dailyData)
     } catch (error) {
       return res.json({ error })
     }
-  }
+  },
 
+  async monthlyData(req, res) {
+    const { id } = req.params
+    const { data } = req.body
+
+    const transformDateInitial = (date) => {
+      const transformedDate = `${date}-01T00:00:00.000Z`
+      return transformedDate
+    }
+
+    const transformDateFinal = (date) => {
+      const transformedDate = `${date}-31T23:59:59.000Z`
+      return transformedDate
+    }
+
+    try {
+      const clinic = await prisma.clinica.findUnique({ where: { id: Number(id) } })
+
+      const consults = await prisma.consulta.findMany({
+        where: {
+          data_de_criacao: {
+            gte: new Date(transformDateInitial(data)),
+            lte: new Date(transformDateFinal(data))
+          }, clinicaId: Number(id)
+        },
+        select: {
+          id: true,
+          data_de_criacao: true,
+          paciente: {
+            select: {
+              nome: true,
+            }
+          },
+          procedimento: {
+            select: {
+              nome: true,
+            }
+          },
+          profissional: {
+            select: {
+              nome: true,
+            },
+          },
+          valor_da_consulta: true,
+          tipo_de_pagamento: true
+        }
+      })
+
+      const numberOfConsults = consults.length
+
+      const totalValue = consults.reduce((acc, curr) => acc + curr.valor_da_consulta, 0)
+
+      function groupConsultsByProfissional(consults) {
+        const consultsByProfissional = {};
+
+        consults.forEach(consult => {
+          const profissionalName = consult.profissional.nome;
+          if (!consultsByProfissional[profissionalName]) {
+            consultsByProfissional[profissionalName] = [];
+          }
+          consultsByProfissional[profissionalName].push(consult);
+        });
+
+        return consultsByProfissional;
+      }
+
+      const consultsByProfissional = groupConsultsByProfissional(consults);
+
+      const valueForProfissional = Object.keys(consultsByProfissional).map(profissional => {
+        const consults = consultsByProfissional[profissional];
+        const numberOfConsults = consults.length;
+        const totalValue = consults.reduce((acc, curr) => acc + curr.valor_da_consulta, 0);
+        return {
+          profissional, numberOfConsults
+        }
+      })
+    } catch (error) {
+      return res.json({ error })
+    }
+
+  },
 
 }
